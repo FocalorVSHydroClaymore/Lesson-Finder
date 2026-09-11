ab6f6fb9afcd9e5837bf10c454293f75a8cb2ec3
FocalorVSHydroClaymore <mark.jpenglish95@gmail.com>
2026-09-11 12:15:26 +0900

    Appscript url change

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Sheet Configurations
 const KEC_SHEET_ID = "16BznwGMZqhWqFIGWIJ1K3DhDcCJr4738byh9zcrhuK8";
 const INTERAC_SHEET_ID = "1WOB0bKFoTlG42vCHmhrcwrCSWSpjD-59dfm3-axKEbs";
-const WEBHOOK_URL = "https://script.google.com/macros/s/AKfycby2FJYZCsgf64UDsNQiLRwBmDtALA8HhYu5kXLe5wJl6vszSSacCsnpqZtQcfe3_aUq/exec";
+const WEBHOOK_URL = "https://script.google.com/macros/s/AKfycbxOCKIEEgZOtlkmqF2GTBdHB4v4XbRxxs8jWRBS91_8VfkyzDcHloUmpOan8ieuVmCQ/exec";
 
 // Dynamically generate current tab name (e.g., "SEPTEMBER 2026")
 function getCurrentTabName() {
